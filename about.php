41cb66a783551412e567d9f219394f8e5eea458e
Bluelight379 <116396669+Bluelight379@users.noreply.github.com>
2026-05-21 12:56:48 +1000

    William - Added documentation to about.php

--- about.php
+++ about.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Need to: -->
-
-<!-- Member contributions and quotes using a definition list -->
-<!-- Include a quote from each member in their first language, along with an English translation. If you are a native English speaker, use a quote in another language you like. -->
-<!-- Group photo - Not individual (< 300KB) using <figure> and caption -->
-<!-- Fun facts table with caption (e.g. dream job, coding snack, hometown) -->
-
-
 <head> <!-- Contains metadata as well as the title of the page -->
 
     <meta charset="UTF-8">
@@ -17,8 +9,9 @@
     <meta name="author" content="William Lloyd">
 
     <title>InfraWatch - About</title> 
-    <link rel="stylesheet" href="styles/styles.css"> 
+    <link rel="stylesheet" href="styles/styles.css"> <!-- Gets the styles.css data for the webpage -->
 
+     <!-- Gets local css data for this webpage -->
     <style>
         .studentid {
             color: gold;
@@ -47,7 +40,7 @@
 
 <body>
 <?php $page_title = "About"; // Set the specific title for this page
-    include 'header.inc'; ?>
+    include 'header.inc'; ?> <!-- gets header data for this page -->
 
     <h1>We are...</h1>
     <ul>
@@ -60,24 +53,24 @@
     <h2>Contributions of the team:</h2>
     <dl> <!--Definition list-->
 
-        <?php
-        require_once("settings.php");
-            $conn = mysqli_connect($host, $username, $password, $database);
+    <?php
+        require_once("settings.php"); // Gets the settings.php for db connection
+        $conn = mysqli_connect($host, $username, $password, $database); // Starts connection to db
         if(!$conn) {
-            echo "<p> Database connection failed". mysqli_connect_error(). "</p>";
+            echo "<p> Database connection failed". mysqli_connect_error(). "</p>"; // if there is no connection, then say connection failed and print out what went wrong
         }
         else {
-            $sql = "SELECT firstname, lastname, studentid, part_1_contributions, part_2_contributions, quote, quote_in_different_lang FROM contributions";
-            $result = mysqli_query($conn, $sql);
-            if($result && mysqli_num_rows($result) > 0){
-                while($row = mysqli_fetch_assoc($result)){
+            $sql = "SELECT firstname, lastname, studentid, part_1_contributions, part_2_contributions, quote, quote_in_different_lang FROM contributions"; //else, get the selection of these elements as a variable
+            $result = mysqli_query($conn, $sql); //get the query of the selection
+            if($result && mysqli_num_rows($result) > 0){ //runs if the database has a result and that the number of rows is not 0
+                while($row = mysqli_fetch_assoc($result)){ //runs for each row
                     $firstname = htmlspecialchars($row['firstname']);
                     $lastname = htmlspecialchars($row['lastname']);
                     $studentid = htmlspecialchars($row['studentid']);
                     $part_1_contributions = htmlspecialchars($row['part_1_contributions']);
                     $part_2_contributions = htmlspecialchars($row['part_2_contributions']);
                     $quote = htmlspecialchars($row['quote']);
-                    $quote_in_different_lang = htmlspecialchars($row['quote_in_different_lang']);
+                    $quote_in_different_lang = htmlspecialchars($row['quote_in_different_lang']); //gets all values to variables
                     echo "
                         <dt>$firstname $lastname: <span class=\"studentid\">$studentid</span></dt>
                         <dd>
@@ -88,12 +81,12 @@
                             <li>\"$quote_in_different_lang\"</li>
                         </ul>
                     <dd>
-                    ";
+                    "; //makes a unsorted list based off of the queries
                 };
             };
-            mysqli_close($conn);
+            mysqli_close($conn); //closes the connection to not cause errors
         }
-        ?>
+    ?>
     </dl>
     
     <figure> <!--For group photo-->
@@ -118,5 +111,5 @@
             </table>
         </figcaption>
     </figure>
-        <?php include 'footer.inc'; ?>
+        <?php include 'footer.inc'; ?> <!-- Adds a footer from footer.inc -->
 </body>
